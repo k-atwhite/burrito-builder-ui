40602663fef81c03738624635cf6b98b56bad87e
Kat White <kat.white.a@gmail.com>
2021-08-10 11:14:46 -0600

    Write draft of tests for form

--- cypress/integration/order_form_spec.js
+++ cypress/integration/order_form_spec.js
@@ -1,4 +1,41 @@
+describe('Home page', () => {
 
-// Form should be visible with all the options
-// You should see that nothing has been selected IF nothing has been selected
+    beforeEach(() => {
+        cy.fixture("mockOrder").then((orderData) => {
+            cy.intercept('http://localhost:3001/api/v1/orders', orderData)
+        })
+        cy.visit('http://localhost:3000')
+    })
+
+    it('User should see the order form', () => {
+        cy.get("form").should('be.visible')
+
+    })
+
+      it('Form should have input for name', () => {
+
+    })
+    
+    it('Form should have buttons all ingredient options', () => {
+
+    })
+
+     it('User should see an error message if they tried to submit without finishing the form', () => {
+
+    })
+
+    it('User should see a button to submit the order', () => {
+    })
+
+    it('User should see that their order was captured after clicking submit', () => {
+    })
+
+    
+})
+
 // You should see a button to click to submit the order
+
+describe("Error handling", () => {
+    it('User should see an error if the post failed', () => {
+    })
+})
